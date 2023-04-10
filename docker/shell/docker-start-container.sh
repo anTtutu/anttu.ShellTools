3ce50409628e649de78169b5f2a791aee179f1b5
Anttu <78537438@qq.com>
2023-04-10 19:30:57 +0800

    modify shell

--- docker/shell/docker-start-container.sh
+++ docker/shell/docker-start-container.sh
@@ -1,13 +1,49 @@
 #!/bin/bash
 
-container_name=$1
+function start()
+{
+    container_name=$1
 
-container_id=$(docker ps -a | grep "${container_name}" | grep "Exited" | awk -F"${container_name}" {'print $1'})
+    if [ "${container_name}" == "all" ]; then
+        # 获取所有状态为 exited 的容器 ID
+        container_ids=$(docker ps -aqf "status=exited")
 
-if [ ! -z "${container_id}" ]; then
-    docker start "${container_id}"
+        # 如果没有容器处于停止状态，则输出提示信息并退出脚本
+        if [ -z "${container_ids}" ]; then
+            echo "No exited containers. Done."
+            exit 0
+        fi
 
-    echo "start successful, container name[${container_name}]"
-else
-    echo "container name[${container_name}] is not exist or started."    
-fi
+        # 循环启动所有处于停止状态的容器
+        for container in ${container_ids}; do
+            docker start ${container}
+        done
+    else
+        container_id=$(docker ps -a | grep ${container_name} | grep "Exited" | awk -F"${container_name}" {'print $1'})
+
+        if [ ! -z ${container_id} ]; then
+            docker start ${container_id}
+
+            echo "start successful, container name[${container_name}]"
+        else
+            echo "container name[${container_name}] is not exist or started."    
+        fi
+    fi
+
+} 
+
+function main()
+{
+    if [ $# -eq 1 ]; then
+        if [ "$1" == "all" ]; then
+            start "all"
+        else
+            start "$1"    
+        fi
+    else
+        echo "Error Parameter, egg: ./docker-start-container mysql or ./docker-start-container all "
+        exit 1
+    fi
+}
+
+main $1
--- docker/shell/docker-stop-container.sh
+++ docker/shell/docker-stop-container.sh
@@ -1,15 +1,49 @@
 #!/bin/bash
 
 
+function stop()
+{
+    container_name=$1
 
-if 
-container_name=$1
+    if [ "${container_name}" == "all" ]; then
+        # 获取所有状态为 running 的容器 ID
+        container_ids=$(docker ps -aqf "status=running")
 
-container_id=$(docker ps -a | grep ${container_name} | grep "Up" | awk -F"${container_name}" {'print $1'})
+        # 如果没有容器处于运行状态，则输出提示信息并退出脚本
+        if [ -z "${container_ids}" ]; then
+            echo "No running containers. Done."
+            exit 0
+        fi
 
-if [ ! -z ${container_id} ]; then
-    docker stop ${container_id}
-    echo "stop successful, container name[${container_name}]"
-else
-    echo "container name[${container_name}] is not exist or stopped."    
-fi
+        # 循环停止所有处于运行状态的容器
+        for container in ${container_ids}; do
+            docker stop ${container}
+        done
+    else
+        container_id=$(docker ps -a | grep "${container_name}" | grep "Up" | awk -F"${container_name}" {'print $1'})
+
+        if [ ! -z ${container_id} ]; then
+            docker stop ${container_id}
+            echo "stop successful, container name[${container_name}]"
+        else
+            echo "container name[${container_name}] is not exist or stopped."    
+        fi
+    fi
+}
+
+
+function main()
+{
+    if [ $# -eq 1 ]; then
+        if [ "$1" == "all" ]; then
+            stop "all"
+        else
+            stop "$1"    
+        fi
+    else
+        echo "Error Parameter, egg: ./docker-stop-container mysql or ./docker-stop-container all "
+        exit 1
+    fi
+}
+
+main $1
